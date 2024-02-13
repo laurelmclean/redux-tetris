a60140bc66b31cf8d66319773bec653ae25594e0
Laurel McLean <laurel.mclean@live.ca>
2024-02-13 16:11:50 -0700

    move left and right

--- src/components/MessagePopup.js
+++ src/components/MessagePopup.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from 'react'
+import { useSelector } from 'react-redux'
 
 // Displays a message
-export default function MessagePopup(props) {
+export default function MessagePopup() {
     const { isRunning, gameOver } = useSelector(state => state)
 
+    let message = ''
+    let isHidden = 'hidden'
+
+    if (gameOver) {
+        message = 'Game Over'
+        isHidden = ''
+    } else if (!isRunning) {
+        message = 'Paused'
+        isHidden = ''
+    }
+
     return (
-        <div className={!isRunning && !gameOver ? 'hidden message-popup' : 'message-popup'}>
-            <h1>Message Title</h1>
-            {!isRunning ? <p>Paused</p> : <p>''</p>}
-            {!gameOver ? <p>Game Over</p> : <p>''</p>}
+        <div className={`message-popup ${isHidden}`}>
+            <h1>{message}</h1>
         </div>
     )
 }
