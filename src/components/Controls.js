964051e44b03be696ce37bb1f883ab5441683209
Laurel McLean <laurel.mclean@live.ca>
2024-02-14 15:16:52 -0700

    timer created

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,10 +1,39 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { moveDown, moveLeft, moveRight, rotate } from '../features/gameSlice'
+import React, { useEffect, useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { moveDown, moveLeft, moveRight, rotate } from '../features/gameSlice';
 
 export default function Controls(props) {
     const dispatch = useDispatch()
-    const { isRunning } = useSelector(state => state)
+    const { isRunning, speed } = useSelector(state => state)
+
+    // Set up the game timer
+    const requestRef = useRef()
+    const lastUpdateTimeRef = useRef(0)
+    const progressTimeRef = useRef(0)
+
+    // Handle game updates to move blocks down the screen
+    const update = (time) => {
+        requestRef.current = requestAnimationFrame(update)
+        if (!isRunning) {
+            return
+        }
+        if (!lastUpdateTimeRef.current) {
+            lastUpdateTimeRef.current = time
+        }
+        const deltaTime = time - lastUpdateTimeRef.current
+        progressTimeRef.current += deltaTime
+        if (progressTimeRef.current > speed) {
+            dispatch(moveDown())
+            progressTimeRef.current = 0
+        }
+        lastUpdateTimeRef.current = time
+    }
+
+    // Initialize request animation frame and remove it when isRunning changes
+    useEffect(() => {
+        requestRef.current = requestAnimationFrame(update)
+        return () => cancelAnimationFrame(requestRef.current)
+    }, [isRunning])
 
     return (
         <div className="controls">
