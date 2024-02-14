4b72c5d6a8de1b5158462d90aa84e1a0caf41b49
Laurel McLean <laurel.mclean@live.ca>
2024-02-14 15:25:36 -0700

    game over implemented

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -4,7 +4,7 @@ import { moveDown, moveLeft, moveRight, rotate } from '../features/gameSlice';
 
 export default function Controls(props) {
     const dispatch = useDispatch()
-    const { isRunning, speed } = useSelector(state => state)
+    const { isRunning, speed, gameOver } = useSelector(state => state)
 
     // Set up the game timer
     const requestRef = useRef()
@@ -39,7 +39,7 @@ export default function Controls(props) {
         <div className="controls">
             {/* left */}
             <button
-                disabled={!isRunning}
+                disabled={!isRunning || gameOver}
                 className="control-button"
                 onClick={(e) => {
                     dispatch(moveLeft())
@@ -48,7 +48,7 @@ export default function Controls(props) {
 
             {/* right */}
             <button
-                disabled={!isRunning}
+                disabled={!isRunning || gameOver}
                 className="control-button"
                 onClick={(e) => {
                     dispatch(moveRight())
@@ -57,7 +57,7 @@ export default function Controls(props) {
 
             {/* rotate */}
             <button
-                disabled={!isRunning}
+                disabled={!isRunning || gameOver}
                 className="control-button"
                 onClick={(e) => {
                     dispatch(rotate())
@@ -66,7 +66,7 @@ export default function Controls(props) {
 
             {/* down */}
             <button
-                disabled={!isRunning}
+                disabled={!isRunning || gameOver}
                 className="control-button"
                 onClick={(e) => {
                     dispatch(moveDown())
